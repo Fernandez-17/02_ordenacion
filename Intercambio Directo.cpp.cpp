a30a6f3367cd843a9f99a7b3d05a74e4130f5139
Fernandez-17 <crfernandezc@unjbg.edu.pe>
2024-09-14 21:51:13 -0500

    Se añadio la opcion de Intercambio directo por la izquierda

--- Intercambio Directo.cpp.cpp
+++ Intercambio Directo.cpp.cpp
@@ -1,47 +1,107 @@
 #include<iostream>
 using namespace std;
-//ordenacion por intercambio directo
+//FUNCIONES PARA REALIZAR LOS TIPOS DE ORDENAMIENTO
 
 void ord_array_derecha(int n, int array[] ){
-	// Por la derecha
+	// por la derecha
 	int aux;
-	for (int i=0 ; i<n-1; i++){
-		for(int j=0; j<n-1-i; j++){
+	for (int i= 0 ; i< n-1 ; i++){
+		for(int j= 0; j< n-i-1 ; j++){
 			if (array[j] > array[j+1]){
-				aux = array[j];
-				array[j] = array [j+1];
+				aux       = array[j];
+				array[j]  = array [j+1];
 				array[j+1]= aux;
 			}
 		}
 	}
 }
 
-int main (){
-	int N, aux;
-	int array[50];
+void ord_array_izquierda(int n, int array[]){
+	//por la izquierda
+	int aux;
+	for(int i= n-1 ; i> 0 ; i--){
+		for(int j= 0 ; j<i ; j++){
+			if(array[j] > array[j+1]){
+				aux        = array[j];
+				array[j]   = array[j+1];
+				array[j+1] = aux;
+			}	
+		}
+	}
+}
 
-	cout <<"   BIENVENIDO AL PROGRAMA DE ORDENACION DE LOS ELEMENTOS DE UN ARREGLO   " << endl;
-	cout << "Ingrese el numero de elementos del arreglo" << endl;
+void solicitar_datos(int &N, int array[]){
+	cout <<"\n   BIENVENIDO AL PROGRAMA DE ORDENACION DE LOS ELEMENTOS DE UN ARREGLO   " << endl;
+	cout <<"\n Ingrese el numero de elementos del arreglo: ";
 	cin  >> N;
+	cout << endl;
 		
-	for (int i = 0; i < N; i++){
+	for (int i = 0; i < N ; i++){
 		cout <<"Elemento["<<i+1<<"]:";
 		cin  >> array[i];
 	} 
-	cout << "Arreglo antes de ordenar"<< endl;
-		for (int i=0 ;i < N; i++){
+	cout << "\n   Arreglo actual   "<< endl;
+		for (int i=0 ; i < N ; i++){
 		cout << array[i]<< " ";
 	}
 	cout << endl;
 	
-	ord_array_derecha (N,array);
 	
-	cout << "Arreglo ordenado: " << endl;
-    for (int i = 0; i < N; i++) {
+}
+
+void mostrar_arreglo_ordenado(int N, int array[]){
+	cout << "\n   Arreglo ordenado " << endl;
+    for (int i = 0; i < N ; i++) {
         cout << array[i] << " ";
     }
-    cout << endl;
+    cout << endl;	
+}
+
+int menu_opciones(){
+	int opcion;
+	cout << "  MENU DE OPCIONES DE ORDENAMIENTO DIRECTO  \n"<<endl;
+	cout <<"1.Intercambio directo por la derecha  " << endl;
+	cout <<"2.Intercambio directo por la izqueirda" << endl;
+	cout <<"3.Intercambio directo con señal       " << endl,
+	cout <<"4.Intercambio directo bidireccional   " << endl;
+	cout <<"5.Salir del programa                  " << endl;
+	cout <<"\nSeleccione un opcion: ";
+	cin  >> opcion;
+	return opcion;
+}
 
 
+int main (){
+	int opcion, N;
+	int array[50];
+	do{
+		opcion = menu_opciones();
+		switch(opcion){
+			case 1:
+				solicitar_datos(N, array);
+				ord_array_derecha(N, array);
+				mostrar_arreglo_ordenado(N, array);
+				break;
+			case 2:
+				solicitar_datos(N, array);
+				ord_array_izquierda(N, array);
+				mostrar_arreglo_ordenado(N, array);
+				break;
+			case 3:
+				//
+				
+				break;
+			case 4:
+				//
+			
+				break;
+			case 5:
+				cout << "\nSALIENDO DEL PROGRAMA .  .  .  " << endl;
+				break;
+			default:
+				cout << "\nOpcion invalida , intentelo de nuevo" << endl;
+				break;	
+		}
+	} while(opcion!=5);
 	return 0;
 }
