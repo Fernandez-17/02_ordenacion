05ce84b2d336d40911f4145132e98ee50f3015d9
rpinor <rpinor@unjbg.edu.pe>
2024-09-20 15:52:48 -0500

    Se añadio el ejercicio de Insercion directa y binaria

--- Insercion directa.cpp
+++ Insercion directa.cpp
@@ -28,7 +28,7 @@ void mostrar_arreglo_ordenado(int array[],int n){
 	cout << endl;
 }
 
-int Funcion_seleccion_directa( int array [],int n){
+int Funcion_insercion_directa( int array [],int n){
 	int aux, k;
 	for ( int i=1 ; i <n ;i++){
 		 aux = array [i];
@@ -41,11 +41,54 @@ int Funcion_seleccion_directa( int array [],int n){
 	}
 }
 
+int Funcion_insercion_binaria( int array [],int n){
+	int aux,izq,der,m,j;
+	for ( int i=1 ; i <n ;i++){
+	    aux = array [i];
+		izq =0;
+		der = i-1;
+		while ( izq <= der){
+			m =(izq + der ) /2;
+			if (aux < array [m]){
+				der = m -1;
+			} else{
+				izq = m + 1;
+			}	
+		}
+		j = i -1 ;
+		while ( j >= izq ){
+			array [j+1] = array [j];
+			j = j--; 
+		}
+		array [izq] = aux;
+	}
+}
+
 int main (){
 	int array[50];
-	int N;
-	solicitar_datos(array,N);
-	Funcion_seleccion_directa(array,N);
-	mostrar_arreglo_ordenado(array,N);
+	int N, opcion;
+	
+	do {
+	cout << "    MENU DE ORDENAMIENTO POR INSERCION " << endl;
+	cout << "1.Mentodo insercion directa" << endl;
+	cout << "2.Metodo insercion binaria " << endl;
+	cout << "3.Salir del programa"        << endl;
+	cout << "\n Seleccione una opcion : ";
+	cin  >> opcion;
+	
+	    switch(opcion){
+		    case 1: 
+		    solicitar_datos(array,N);
+			Funcion_insercion_directa(array,N);
+			mostrar_arreglo_ordenado(array,N);
+			break;
+		    case 2: 
+			solicitar_datos(array,N);
+			Funcion_insercion_binaria(array,N);
+			mostrar_arreglo_ordenado(array,N);
+		    case 3:
+			cout << "    Saliendo del programa .  .  .  " << endl;
+			}
+		} while (opcion !=3);
 	return 0;
 }
